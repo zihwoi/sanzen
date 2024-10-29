6021f799859e7958161f61bc5ca95d607a63594c
zihwoi <72529512+zihwoi@users.noreply.github.com>
2024-10-29 12:49:13 +0800

    Update app firebase

--- sanzen/src/App.js
+++ sanzen/src/App.js
@@ -1,37 +1,24 @@
-// src/App.js
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './components/Dashboard';
 import AddTransaction from './pages/AddTransaction';
-import Navigation from './components/Navigation';
 import { GlobalProvider } from './context/GlobalState';
-import BudgetOverview from './components/BudgetOverview'; // Adjust the path as necessary
+import BudgetOverview from './components/BudgetOverview';
+import NotFound from './pages/NotFound'; // Optional
 import './App.css'; // Import global CSS here
 
-
 function App() {
-  const [expenses, setExpenses] = useState([]);
-  const [incomes, setIncomes] = useState([]);
-
-  const totalIncome = incomes.reduce((total, income) => total + income.amount, 0);
-  const totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-
-  const addExpense = (expense) => setExpenses([...expenses, expense]);
-  const addIncome = (income) => setIncomes([...incomes, income]);
-
   return (
     <GlobalProvider>
       <Router>
         <div className="app">
-          <Navigation />
           <nav className="nav">
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/dashboard">Dashboard</Link></li>
               <li><Link to="/add-transaction">Add Transaction</Link></li>
               <li><Link to="/budget-overview">Budget Overview</Link></li>
-
             </ul>
           </nav>
 
@@ -39,25 +26,10 @@ function App() {
           <main className="main-content">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route
-                path="/dashboard"
-                element={
-                  <Dashboard
-                    totalIncome={totalIncome}
-                    totalExpenses={totalExpenses}
-                  />
-                }
-              />
-              <Route
-                path="/add-transaction"
-                element={
-                  <AddTransaction
-                    addIncome={addIncome}
-                    addExpense={addExpense}
-                  />
-                }
-              />
-              <Route path="/budget-overview" element={<BudgetOverview />} /> {/* Add the new route */}
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/add-transaction" element={<AddTransaction />} />
+              <Route path="/budget-overview" element={<BudgetOverview />} />
+              <Route path="*" element={<NotFound />} /> {/* Optional */}
             </Routes>
           </main>
         </div>
